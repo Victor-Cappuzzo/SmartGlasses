af93b9495f6b1130757efee546530c5c02856d00
Destructor3k <vdestructor3k@gmail.com>
2023-06-14 17:15:00 -0400

    Completed Phase 2
    OLED display
    RPI bluetooth
    Android App

--- Android App/app/src/main/java/com/example/smartglassesapp/MainActivity.kt
+++ Android App/app/src/main/java/com/example/smartglassesapp/MainActivity.kt
@@ -1,11 +1,113 @@
 package com.example.smartglassesapp
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.smartglassesapp.databinding.ActivityMainBinding
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Variable declaration
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var bluetoothSocket: BluetoothSocket
+    private lateinit var outputStream: OutputStream
+    private lateinit var bluetoothDevice: BluetoothDevice
+    private lateinit var binding: ActivityMainBinding
+    private var messageEditText: EditText? = null
+    private var sendButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Variable initialization
+        messageEditText = binding.messageEditText
+        sendButton = binding.sendButton
+
+        // Set up bluetooth adapter
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        // Get all devices that are paired via bluetooth with the phone
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter.bondedDevices
+
+        // Find the HC-06 board as the bluetooth device
+        pairedDevices.let {
+            for (device: BluetoothDevice in it!!) {
+
+                // Permissions check
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+
+                // If the devices name is "HC-06", then set it as our device
+                if (device.name == "HC-06") {
+                    bluetoothDevice = device
+                    break
+                }
+            }
+        }
+
+        // Listen for if the send button is pressed
+        sendButton!!.setOnClickListener {
+            val message = messageEditText!!.text.toString()
+
+            // Start thread to send messages
+            //SendMessageThread(message).start()
+            SendMessageThread(message).start()
+        }
+    }
+
+    inner class SendMessageThread(val message: String): Thread() {
+        override fun run() {
+            try {
+                // HC-06 UUID
+                val uuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+
+                // Set up and connect socket
+                bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuid)
+                bluetoothSocket.connect()
+
+                // Send the message to the HC-06
+                outputStream = bluetoothSocket.outputStream
+                outputStream.write((message + "\n").toByteArray())
+                outputStream.flush()
+                outputStream.close()
+                bluetoothSocket.close()
+
+                // Display toast to indicate that data was send
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "Message sent: $message", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
--- Raspberry Pi Pico W Code/main.py
+++ Raspberry Pi Pico W Code/main.py
@@ -1,16 +1,23 @@
-from machine import Pin, SPI
+from machine import Pin, SPI, UART
 from ssd1309 import Display
 from xglcd_font import XglcdFont
 from time import sleep
 
-# Define pins
+# Define pins for OLED
 din_pin = machine.Pin(19)
 clk_pin = machine.Pin(18)
 cs_pin = machine.Pin(17)
 dc_pin = machine.Pin(16)
 rst_pin = machine.Pin(20)
 
-# Define the SPI bus
+# Define pins for UART
+tx_pin = machine.Pin(0)
+rx_pin = machine.Pin(1)
+
+# Define UART object on UART0 ports
+uart = UART(0, baudrate=9600, tx=tx_pin, rx=rx_pin)
+
+# Define the SPI bus on SPI0 ports
 spi = SPI(0, sck=clk_pin, mosi=din_pin)
 
 # Create an SSD1309 OLED object
@@ -19,12 +26,25 @@ oled = Display(spi, dc=dc_pin, cs=cs_pin, rst=rst_pin)
 # Font
 font = XglcdFont('fonts/ArcadePix9x11.c', 9, 11)
 
-# Display stuff to the OLED
-text = "Victor"
-text_len = font.measure_text(text)
-oled.draw_text(oled.width//2 , oled.height//2 - text_len//2, text, font, rotate=90)
-oled.present()
+# Recieve UART messages
+while True:
+    
+    # If we have received a message
+    if uart.any():
+        
+        sleep(0.1)
+        
+        # Grab the message
+        message = uart.readline().decode().strip()  # Decode the received bytes and remove whitespace
+        message = message.replace('\r', '')  # Remove carriage return character
+        print(message)
+        
+        # Display message to the OLED
+        text_len = font.measure_text(message)
+        oled.draw_text(oled.width//2 , oled.height//2 - text_len//2, message, font, rotate=180)
+        oled.present()
 
-sleep(10)
+        sleep(10)
 
-oled.cleanup()
+        oled.cleanup()
+        
